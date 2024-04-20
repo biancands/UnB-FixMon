519518121fe9799aed2eb09533a3d2f323d2c5d1
Marcjuniu09 <marcjuniu@gmail.com>
2024-04-20 11:55:53 -0300

    adiciona domains headers data percentual

--- library/domains.cpp
+++ library/domains.cpp
@@ -1,1 +1,87 @@
 #include "domains.h"
+#include <iostream>
+#include <sstream>
+#include <stdexcept>
+
+Data::Data() : dia(0), mes(0), ano(0) {}
+
+Data::Data(const std::string& data) {
+    setData(data);
+}
+
+void Data::setData(const std::string& data) {
+    std::stringstream ss(data);
+    char delim;
+    ss >> dia >> delim >> mes >> delim >> ano;
+    if (ss.fail() || ss.peek() != EOF) {
+        throw std::length_error("Erro: Formato de data invalido");
+        exit(1);
+        }
+
+    validarDia(dia, mes);
+    validarMes(mes);
+    validarAno(ano);
+}
+
+int Data::getDia() const {
+    return dia;
+}
+
+int Data::getMes() const {
+    return mes;
+}
+
+int Data::getAno() const {
+    return ano;
+}
+
+void Data::validarDia(int d, int m) {
+    if (d < 1 || ((m == 4 || m == 6 || m == 9 || m == 11) && d > 30) || (m == 2 && ((ehBissexto(ano) && d > 29) || (!ehBissexto(ano) && d > 28))) || (m != 2 && d > 31)) {
+        throw std::invalid_argument("Erro: Dia invalido");
+        exit(1);
+    }
+}
+
+void Data::validarMes(int m) {
+    if (m < 1 || m > 12) {
+        throw std::invalid_argument("Erro: Mes invalido");
+        exit(1);
+    }
+}
+
+void Data::validarAno(int a) {
+    if (a < 2000 || a > 2100) {
+        throw std::length_error("Erro: Ano invalido");
+        exit(1);
+    }
+}
+
+bool Data::ehBissexto(int a) {
+    return (a % 4 == 0 && a % 100 != 0) || (a % 400 == 0);
+}
+
+
+
+//Definição de métodos percentual
+void Percentual::setValor(int v) {
+    validarValor(v);
+    valor = v;
+}
+
+int Percentual::getValor() const {
+    return valor;
+    }
+
+void Percentual::validarValor(int v){
+    if(v < 0 || v > 100 ){
+        throw std::length_error("Erro: Valor invalido");
+        exit(1);
+    }
+}
+
+
+
+
+
+
+
--- library/domains.h
+++ library/domains.h
@@ -1,6 +1,35 @@
-#ifndef DOMAINS_H_INCLUDED
-#define DOMAINS_H_INCLUDED
+#ifndef DOMAINS_H
+#define DOMAINS_H
+
+#include <string>
+
+class Data {
+private:
+    int dia, mes, ano;
+    void validarDia(int, int);
+    void validarMes(int);
+    void validarAno(int);
+    bool ehBissexto(int);
+
+public:
+    Data();
+    Data(const std::string&);
+    void setData(const std::string&);
+    int getDia() const;
+    int getMes() const;
+    int getAno() const;
+};
 
 
 
-#endif // DOMAINS_H_INCLUDED
+class Percentual {
+private:
+    int valor;
+    void validarValor(int);
+public:
+    void setValor(int);
+    int getValor() const;
+};
+
+#endif // DOMAINS_H
+
