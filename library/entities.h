efe071446324a6841a4af096d1c9ce15a741911f
Nilo Silva <danilo.silveira.silv@gmail.com>
2024-05-06 16:38:55 -0300

    push: entenidade pagamento

--- library/entities.h
+++ library/entities.h
@@ -7,5 +7,55 @@
 
 using namespace std;
 
+//222014142
+class Pagamento {
+private:
+    CodigoPagamento codigo;
+    Data data;
+    Percentual percentual;
+    Estado estado;
+public:
+    void setCodigo(const CodigoPagamento&);
+    CodigoPagamento getCodigo() const;
+    void setData(const Data&);
+    Data getData() const;
+    void setPercentual(const Percentual&);
+    Percentual getPercentual() const;
+    void setEstado(const Estado&);
+    Estado getEstado() const;
+};
+
+inline void Pagamento::setCodigo(const CodigoPagamento& codigo){
+    this->codigo = codigo;
+}
+
+inline CodigoPagamento Pagamento::getCodigo() const {
+    return codigo;
+}
+
+inline void Pagamento::setData(const Data& data){
+    this->data = data;
+}
+
+inline Data Pagamento::getData() const {
+    return data;
+}
+
+inline void Pagamento::setPercentual(const Percentual& percentual){
+    this->percentual = percentual;
+}
+
+inline Percentual Pagamento::getPercentual() const {
+    return percentual;
+}
+
+inline void Pagamento::setEstado(const Estado& estado){
+    this->estado = estado;
+}
+
+inline Estado Pagamento::getEstado() const {
+    return estado;
+}
+
 
 #endif // ENTITIES_H_INCLUDED
