64bf5d5ac5a787b4a4f6d2fce5491df36b3f41ad
flbparra <fabiolbparra@gmail.com>
2024-07-31 13:13:29 -0300

    fix: add arquivos que vao ser ignorados no gitignore

--- system/ctrl_servico.cpp
+++ system/ctrl_servico.cpp
@@ -2,3 +2,62 @@
 #include "comandos.h"
 
 using namespace std;
+
+bool cntrISConta::criar(const Conta& conta) {
+
+    for(const auto& c : contas) {
+        if(c.getNumCPF().getCPF() == conta.getNumCPF().getCPF()) {
+
+            cout << "Error: CPF ja existe no Banco de Dados" << endl;
+            return false;
+        }
+    }
+
+    contas.push_back(conta);
+    cout << "Sucesso: Conta criada!" << endl;
+    return true;
+}
+
+bool cntrISConta::ler(Conta* conta){
+
+    for(const auto& c : contas){
+        if(c.getNumCPF().getCPF() == conta->getNumCPF().getCPF()){
+            *conta = c;
+            cout << "Sucesso: Dados da conta lidos com sucesso" << endl;
+            return true;
+        }
+    }
+
+    cout << "Error: Conta não encontrada" << endl;
+    return false;
+}
+
+
+bool cntrISConta::atualizar(const Conta& conta) {
+
+    for(auto& c : contas){
+
+        if(c.getNumCPF().getCPF() == conta->getNumCPF().getCPF()){
+            c = conta;
+            cout << "Sucesso: Conta atualizada com sucesso!" << endl;
+            return true;
+        }
+    }
+
+    cout << "Error: Conta nao encontrada" << endl;
+    return false;
+}
+
+bool cntrISConta::excluir(const CPF& cpf){
+    auto it = remove_if(contas.begin(), contas.end(), [&](const Conta& c) {
+        return c.getNumCPF().getCPF() == cpf.getCPF();
+    });
+
+    if (it != contas.end()) {
+        contas.erase(it, contas.end());
+        cout << "Conta excluída com sucesso!" << endl;
+        return true;
+    }
+    cout << "Erro: Conta não encontrada." << endl;
+    return false;
+}
--- system/ctrl_servico.h
+++ system/ctrl_servico.h
@@ -6,9 +6,20 @@
 
 #include <stdexcept>
 #include <iostream>
+#include <vector>
 #include <cstdlib>
 
 using namespace std;
 
+class cntrISConta: public ISConta{
+
+    private:
+        vector<Conta> contas;  //banco improvisado simulando contas
+    public:
+        bool criar(const Conta&);
+        bool ler(Conta*);
+        bool atualizar(const Conta&);
+        bool excluir(const CPF&);   
+};
 
 #endif // CTRL_SERVICO_H_INCLUDED
